902fb128421f8aac2d2a058174f6ae3c1fc46fb1
Valery Zhidkow <vjpaps793@gmail.com>
2024-10-27 13:18:46 +0300

    Rounding coordinates and readme changes

--- gps_coordinates.py
+++ gps_coordinates.py
@@ -1,45 +1,55 @@
 # Модуль получения координат
 
 from dataclasses import dataclass
+import config
 from exception import IncorrectCoordinateInput
 
-# применяем slots и frozen для преобразования к компактному хранению и если нужен read only объект
-# @dataclass(slots=True, frozen=True)
-@dataclass()         #  без slots и frozen - not read only object
+# Типизируем получаемые координаты
+# применяем slots и frozen для преобразования к компактному хранению и если нужен read only object
+# без slots и frozen - not read only object
+@dataclass(slots=True, frozen=True)
+
 class Coordinates:
-    longitude: float
     latitude: float
+    longitude: float
+
 def get_gps_coordinates() -> Coordinates:
-    # Координаты по умолчанию: 48.403121, 40.280044
-    coords = Coordinates(longitude=48.403121, latitude=40.280044)
+    # Инициализируем координатами по умолчанию:
+    latitude,  longitude = (config.LATITUDE,  config.LONGITUDE)
 
+    # Получаем широту
     print('Введите широту в формате [[+/-]##[.######]] в диапазоне от -90 до +90:')
-    string_inp = input().strip()
-    if len(string_inp) == 0:
-        print('Выбрано значение широты по-умолчанию: 48.403121')
+    string_input = input().strip()
+    if len(string_input) == 0:
+        print(f'Выбрано значение широты по-умолчанию: {config.LATITUDE}')
     else:
         try:
-            coords.latitude = float(string_inp)
+            latitude = float(string_input)
         except ValueError:
-            print('Введено не число.')
+            print('Ошибка: введено не число.')
             raise IncorrectCoordinateInput
-        if coords.latitude < -90 or coords.latitude > 90:
+        if latitude < -90 or latitude > 90:
             raise IncorrectCoordinateInput
 
+    # Получаем долготу
     print('Введите долготу в формате [##[.######]] в диапазоне от -180 до +180:')
-    string_inp = input().strip()
-    if len(string_inp) == 0:
-        print('Выбрано значение долготы по-умолчанию: 40.280044')
+    string_input = input().strip()
+    if len(string_input) == 0:
+        print(f'Выбрано значение долготы по-умолчанию: {config.LONGITUDE}')
     else:
         try:
-            coords.longitude = float(string_inp)
+            longitude = float(string_input)
         except ValueError:
-            print('Введено не число.')
+            print('Ошибка: введено не число.')
             raise IncorrectCoordinateInput
-        if coords.longitude < -180 or coords.longitude > 180:
+        if longitude < -180 or longitude > 180:
             raise IncorrectCoordinateInput
 
-    return coords
+    # округляем если задано в настройках
+    if config.USE_ROUNDED_COORDS:
+        latitude, longitude = map(lambda c: round(c, 1), [latitude, longitude])
+
+    return Coordinates(latitude=latitude, longitude=longitude)
 
 if __name__ == "__main__":
     print(get_gps_coordinates())
