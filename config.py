91318db34e67d46e0048417fa345c50304acc1f1
Valery Zhidkow <vjpaps793@gmail.com>
2024-10-28 21:51:42 +0300

    Close api-key and apply env

--- config.py
+++ config.py
@@ -1,17 +1,41 @@
-# Модуль конфигурации применения координат: точные/примерные
+# Модуль конфигурации
+
+__doc__='''
+Модуль конфигурации
+Хранение ключа в .env фале переменных среды окружения.
+Содержимое файла .env для корректной работы приложения:
+    openweathet_api=<Ваш API-ключ доступа к OpenWeatherApi>
+    env_example_list=1,2,3,4,5
+    env_example_dict=A=Hello,B=Bye bye
+'''
+
+import environ
 
 # Флаг точного/приблизительного применения координат (False/True)
 USE_ROUNDED_COORDS = False
 
-# Координаты по умолчанию
+# Координаты по умолчаниюd
 LATITUDE = 48.403121
 LONGITUDE = 40.280044
 
+# чтение секретного api-ключа из файла перенменных среды окружения
+env = environ.Env()
+environ.Env.read_env()
+OPENWEATHER_API = env('openweather_api', str)
+
 # Шаблоны для доступа к API сервиса OpenWeather
-OPENWEATHER_API = "3915122e704b3f5950da77e4959343bc"
 OPENWEATHER_URL = (
 "https://api.openweathermap.org/data/2.5/weather?"
 "lat={latitude}&lon={longitude}&"
 "appid=" + OPENWEATHER_API + "&lang=ru&"
 "units=metric"
 )
+
+if __name__ == "__main__":
+    # демо учебных переменных
+    ENV_EXAMPLE_LIST = env('env_example_list', list)
+    EMV_EXAMPLE_DICT = env('env_example_dict', dict)
+    print('Пример работы c переменными среды с помощью библиотеки django_environ:')
+    print(f'Итоговый запрос к сервису погоды: {OPENWEATHER_URL}')
+    print(f'Переменная среды - список: {ENV_EXAMPLE_LIST}')
+    print(f'Переменная среды - словарь: {EMV_EXAMPLE_DICT}')
